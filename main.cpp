b31fb75b41d035452369893d2cfd77c28832968a
Zhou Yanyu（周妍妤） <earthazyy@hotmail.com>
2023-04-23 20:39:50 +0200

    delete weight from Edge;
    不在buildGraph里算距离，在prim算法里算距离

--- main.cpp
+++ main.cpp
@@ -17,15 +17,16 @@ const int R = 6378; // radius of the earth in km
 struct Edge {
     int city;
     int parent;
-    double weight;
+    // double weight;
 
-    bool operator>(const Edge& other) const {
-        return weight > other.weight;
-    };
+    //bool operator>(const Edge& other) const {
+    //    return weight > other.weight;
+    //};
 };
 
 typedef vector<vector<Edge>> Graph;
 
+/*
 Graph buildGraph(const ListOfCities& cities) {
     Graph graph(cities.number);
 #pragma omp parallel for default(none) shared(cities,graph)
@@ -46,7 +47,33 @@ Graph buildGraph(const ListOfCities& cities) {
     }
 
     return graph;
+}*/
+
+double calculateDistance(const ListOfCities& cities, int city1, int city2) {
+    double lat1 = cities.lat[city1] * M_PI / 180.0;
+    double lon1 = cities.lon[city1] * M_PI / 180.0;
+    double lat2 = cities.lat[city2] * M_PI / 180.0;
+    double lon2 = cities.lon[city2] * M_PI / 180.0;
+
+    // Calculate the distance between two cities
+    return acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2)) * R;
 }
+
+Graph buildGraph(const ListOfCities& cities) {
+    Graph graph(cities.number);
+
+#pragma omp parallel for default(none) shared(cities,graph)
+    for (int i = 0; i < cities.number; ++i) {
+        for (int j = 0; j < cities.number; ++j) {
+            if (i != j) {
+                graph[i].push_back(Edge{j, i});
+            }
+        }
+    }
+
+    return graph;
+}
+
 
 Graph buildGraph_cap(const ListOfCities& cities, map<int,Department*> map_dep) {
     Graph graph(map_dep.size());
@@ -58,8 +85,11 @@ Graph buildGraph_cap(const ListOfCities& cities, map<int,Department*> map_dep) {
         int cap_i = it1->second->getCapital(); //it1->second Departement*
         for (auto it2 = map_dep.begin(); it2 != map_dep.end(); it2++) {
             if (i != j) {
+                graph[i].push_back(Edge{j, i});
+                /*
                 int cap_j = it2->second->getCapital();
                 // Convert degrees to radians
+                
                 double lat1 = cities.lat[cap_i] * M_PI / 180.0;
                 double lon1 = cities.lon[cap_i] * M_PI / 180.0;
                 double lat2 = cities.lat[cap_j] * M_PI / 180.0;
@@ -68,6 +98,7 @@ Graph buildGraph_cap(const ListOfCities& cities, map<int,Department*> map_dep) {
                 // Calculate the distance between two cities
                 double distance = acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2)) * R;
                 graph[i].push_back(Edge{cap_j, cap_i, distance});
+                */
             }
         }
         return graph;
@@ -77,6 +108,7 @@ Graph buildGraph_cap(const ListOfCities& cities, map<int,Department*> map_dep) {
 }
 
 // prim algorithm
+/*
 pair<double, vector<pair<int,int>>> prim(const Graph& graph){
     vector<bool> visited(graph.size(), false);
     vector<pair<int,int>> edges;
@@ -107,7 +139,43 @@ pair<double, vector<pair<int,int>>> prim(const Graph& graph){
         }
     }
     return {totalWeight, edges};
+}*/
+
+pair<double, vector<pair<int,int>>> prim(const ListOfCities& cities, const Graph& graph){
+    vector<bool> visited(cities.number, false);
+    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
+
+    visited[0] = true;
+    for (const Edge& e : graph[0]){
+        pq.push(e);
+    }
+
+    double totalWeight = 0.0;
+    vector<pair<int,int>> treeEdges;
+
+    while (!pq.empty()){
+        Edge current = pq.top();
+        pq.pop();
+        int city = current.city;
+        int parent = current.parent;
+
+        if (visited[city]){
+            continue;
+        } else {
+            visited[city] = true;
+            totalWeight += calculateDistance(cities, city, parent);
+            treeEdges.push_back(make_pair(parent, city));
+
+            for (const Edge& edge : graph[city]){
+                if (!visited[edge.city]) {
+                    pq.push(edge);
+                }
+            }
+        }
+    }
+    return {totalWeight, treeEdges};
 }
+
 
 // prim algorithm
 pair<double, vector<pair<int,int>>> prim_bloc(const ListOfCities& cities){
@@ -124,8 +192,20 @@ pair<double, vector<pair<int,int>>> prim_bloc(const ListOfCities& cities){
         }
     }
 
+    // calculate MST for each department
+    for (auto it = map_dep.begin(); it != map_dep.end(); it++) {
+        int dep_i = it->first;
+        ListOfCities cities_dep = it->second->getCities();
+        Graph graph = buildGraph(cities_dep);
+        pair<double, vector<pair<int,int>>> result = prim(graph);
+        it->second->setMST(result);
+    }
+
+    // calculate MST for department captials
     auto tree_cap = prim(buildGraph_cap(cities,map_dep));
-    
+
+    // combine the MSTs and return the result
+
 
 
 }
